7705214f9999450274d1367c0147a3ecb9766eb5
holyporing1 <sthananon@gmail.com>
2019-12-01 13:49:21 +0700

    First tutorial commit

---
+++ app/src/main/java/site/sterk/weatherforecastmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package site.sterk.weatherforecastmvvm.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : Fragment() {
+
+
+}
---
+++ app/src/main/java/site/sterk/weatherforecastmvvm/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package site.sterk.weatherforecastmvvm.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
